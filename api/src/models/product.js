5abb12d4b93d9059479debac54f1dfea0fcbf046
Abhishek Silawat <absilawat@gmail.com>
2022-11-19 20:10:06 +0530

    Create product.js

---
+++ api/src/models/product.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    category: { type: Array },
+    size: { type: Array },
+    color: { type: String },
+    price: { type: String, required: true },
+    inStock: { type: Boolean, default: true },
+  },
+  { timeStamps: true, versionKey: false }
+);
+
+const Product = model("product", productSchema);
+module.exports = Product;
