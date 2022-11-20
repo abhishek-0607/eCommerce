ab4a084eafe00b18e17df8254673817553ba5494
Abhishek Silawat <absilawat@gmail.com>
2022-11-20 20:36:20 +0530

    Update product.js

--- api/src/models/product.js
+++ api/src/models/product.js
@@ -5,7 +5,7 @@ const productSchema = new Schema(
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
-    category: { type: Array },
+    categories: { type: Array },
     size: { type: Array },
     color: { type: String },
     price: { type: String, required: true },
