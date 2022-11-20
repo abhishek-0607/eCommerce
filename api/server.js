6115de1faaecd40b982b5543d0d47ea18d7cb00b
Abhishek Silawat <absilawat@gmail.com>
2022-11-20 20:35:31 +0530

    basic setup of server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
-
 const connect = require("./src/configs/db");
+require("dotenv").config();
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   connect();
   console.log("Listening to PORT 8080");
 });
