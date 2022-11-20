61c7fc7c9ae28ba053897c4fd6fd733fa70a5949
Abhishek Silawat <absilawat@gmail.com>
2022-11-20 20:35:56 +0530

    added userController and authController

--- api/app.js
+++ api/app.js
@@ -2,5 +2,10 @@ const express = require("express");
 const connect = require("./src/configs/db");
 const app = express();
 app.use(express.json());
+const userController = require("./src/controllers/userController");
+const authController = require("./src/controllers/authController");
+
+app.use("/api", userController);
+app.use("/api", authController);
 
 module.exports = app;
