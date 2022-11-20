4e6450aaab1d98b10e87a6d1bcfbf9df1a5bd0f3
Abhishek Silawat <absilawat@gmail.com>
2022-11-20 20:36:24 +0530

    Update user.js

---
+++ api/src/models/user.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
