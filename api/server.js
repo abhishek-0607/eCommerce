d8e98491c173c13b4b400e6e50a8e1f303005472
Abhishek Silawat <absilawat@gmail.com>
2022-11-22 00:33:20 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const connect = require("./src/configs/db");
 require("dotenv").config();
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 5000, () => {
   connect();
   console.log("Listening to PORT 8080");
 });
