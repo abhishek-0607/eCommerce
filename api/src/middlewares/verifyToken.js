d8505849ab592183a017d6967a9fab3b073e48b2
Abhishek Silawat <absilawat@gmail.com>
2022-11-23 22:13:56 +0530

    Create verifyToken.js

---
+++ api/src/middlewares/verifyToken.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json("token is not valid!");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("you are not authenticated");
+  }
+};
+const authorize = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("you are not allowed to do that");
+    }
+  });
+};
+
+const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("you are not allowed to do that");
+    }
+  });
+};
+module.exports = { verifyToken, authorize, verifyAdmin };
