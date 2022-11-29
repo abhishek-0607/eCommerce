815808531f8b01f6a6462490fac0d158d5892cf5
Abhishek Silawat <absilawat@gmail.com>
2022-11-29 12:00:51 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -5,10 +5,14 @@ const userController = require("./src/controllers/userController");
 const authController = require("./src/controllers/authController");
 
 const productController = require("./src/controllers/productController");
+const cartController = require("./src/controllers/cartController");
+const orderController = require("./src/controllers/orderController");
 
 app.use("/api/users", userController);
 app.use("/api", authController);
 
-app.use("/products", productController);
+app.use("/api/products", productController);
+app.use("/api/carts", cartController);
+app.use("/api/orders", orderController);
 
 module.exports = app;
