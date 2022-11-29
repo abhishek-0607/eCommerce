b40f28ea9b7cd0b13b56d6d3959c20b6e396d0d1
Abhishek Silawat <absilawat@gmail.com>
2022-11-29 12:02:21 +0530

    create required routes

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { ProductList } from "./pages/ProductList";
+import { Product } from "./pages/Product";
+import { Cart } from "./pages/Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />}>
+            <Route path="/products/:category" element={<ProductList />} />
+            <Route path="/product/:id" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
