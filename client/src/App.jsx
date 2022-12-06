6d5f984c8aa4ac562e68ed7f890cfecedd878edf
Abhishek Silawat <absilawat@gmail.com>
2022-12-06 18:50:03 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,15 +10,16 @@ import { Cart } from "./pages/Cart";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />}>
-            <Route path="/products/:category" element={<ProductList />} />
-            <Route path="/product/:id" element={<Product />} />
-          </Route>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/products/:category" element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />} />
+
           <Route path="/cart" element={<Cart />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Product />
     </div>
   );
 }
