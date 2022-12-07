ae46a55df02fcec8b44d921e6570317de661a986
Abhishek Silawat <absilawat@gmail.com>
2022-12-07 23:53:03 +0530

    Update Product.jsx

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -5,13 +5,59 @@ import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import { Newsletter } from "../components/Newsletter";
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.div``;
-const InfoContainer = styled.div``;
-const Title = styled.div``;
-const Desc = styled.div``;
-const Price = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.div`
+  font-weight: 100;
+  font-size: 40px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  margin: 30px 0px;
+  justify-content: space-between;
+  width: 50%;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
 export const Product = () => {
   return (
     <Container>
@@ -19,12 +65,36 @@ export const Product = () => {
       <Announcement />
       <Wrapper>
         <ImgContainer>
-          <Image></Image>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"></Image>
         </ImgContainer>
         <InfoContainer>
-          <Title></Title>
-          <Desc></Desc>
-          <Price></Price>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+            tristique tortor pretium ut. Curabitur elit justo, consequat id
+            condimentum ac, volutpat ornare.
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
