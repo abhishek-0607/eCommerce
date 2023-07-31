428c80f7bdaa8ee9518e99c41a6b1a8cd7b66f3c
Abhishek Silawat <absilawat@gmail.com>
2023-07-31 14:43:57 +0530

    getting products from API

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import { Product } from "./Product";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 const Container = styled.div`
   display: flex;
   padding: 20px;
@@ -9,7 +12,19 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 `;
-export const Products = () => {
+export const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/products");
+        console.log(res);
+      } catch (error) {}
+    };
+    getProducts();
+  }, [cat]);
   return (
     <Container>
       {popularProducts.map((item) => (
